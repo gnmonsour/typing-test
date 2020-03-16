a8f5a1c76bee02e456d867f70864ff8fca8c4e85
George N Monsour <gnmonsour@gmail.com>
2020-03-16 18:49:05 -0400

    working without error highlight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'typing';
+export class AppComponent implements OnInit{
+  title = 'Typing Challenge';
+  testText: string = '';
+  answerText: string = '';
+  isSame: boolean = false;
+
+
+  ngOnInit(): void {
+    this.testText = lorem.sentence();
+
+  }
+
+  markAnswer(val: string) {
+    this.answerText = val;
+    if(this.testText === val) {
+      this.isSame = true;
+    } else {
+      this.isSame = false;
+    }
+  }
 }
